3475594859ae3d36c0cb85adaff63d3d43693b6c
bobchien <bob.sc.chien@gmail.com>
2022-03-07 23:54:53 +0800

    fix important bug which is from ZKCF_normalize parameter

--- src/make_tokenizer.py
+++ src/make_tokenizer.py
@@ -6,29 +6,37 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+from utils import configuration
 from utils.configuration import *
 from utils.preprocessor import preprocessors, cc_converter
 from data.dataset_pipeline import setup_tfds_builder
 from models.tokenizer import RESERVED_TOKENS, build_bert_tokenizer, CustomTokenizer
 
+changeToColabPath(configuration.colab)
+createDirectory()
 
 config = configparser.ConfigParser()
 config.read('../config/model.cfg')
 lang = config['data']['lang']
 
-vocab_path = os.path.join(DIR_VOCAB, f'{lang}_vocab.txt')
+vocab_path = os.path.join(configuration.DIR_VOCAB, f'{lang}_vocab.txt')
 
 
 if __name__ == '__main__':
     
     ### build lexicon in tf.Dataset object & build the vocabularies
     
+    if lang in ['zh', 'zhc', 'jp', 'kr']:
+        cjk = True
+    else:
+        cjk = False
+
     # basic lexicon
     
     if lang == 'zh':
         # Classical Chinese
-        builder = tfds.builder("wikipedia/20190301.zh-classical", data_dir=DIR_DATA)
-        builder.download_and_prepare(download_dir=DIR_DATA)
+        builder = tfds.builder("wikipedia/20190301.zh-classical", data_dir=configuration.DIR_DATA)
+        builder.download_and_prepare(download_dir=configuration.DIR_DATA)
         lexicon_zhc, _, _ = setup_tfds_builder(builder, pcts=((100, 0), 0), as_supervised=False)
         lexicon_zhc = lexicon_zhc.map(lambda text:text['text']). \
                                   map(preprocessors[lang])
@@ -42,32 +50,30 @@ if __name__ == '__main__':
             tfds.Split.VALIDATION: ["newstest2018"]
           }
         )
-        builder = tfds.builder("wmt_translate", data_dir=DIR_DATA, config=config)
-        builder.download_and_prepare(download_dir=DIR_DATA)
+        builder = tfds.builder("wmt_translate", data_dir=configuration.DIR_DATA, config=config)
+        builder.download_and_prepare(download_dir=configuration.DIR_DATA)
 
         lexicon_zh, _, _ = setup_tfds_builder(builder, pcts=((100, 0), 0), as_supervised=True)
         lexicon_zh = lexicon_zh.map(lambda zh,en:tf.py_function(func=cc_converter, inp=[zh], Tout=tf.string)). \
                                 map(preprocessors[lang])
         
-        #.concatenate(dataset_zhc)
-
     # training lexicon
     
-    lexicon = pd.concat([pd.read_csv(file) for file in glob.glob(os.path.join(DIR_INTERMIN, lang, '*'))]).dropna()
+    lexicon = pd.concat([pd.read_csv(file) for file in glob.glob(os.path.join(configuration.DIR_INTERMIN, lang, '*'))]).dropna()
     lexicon = np.concatenate([lexicon.target.values, lexicon.source.values])
     lexicon = tf.data.Dataset.from_tensor_slices(lexicon.tolist()). \
                               map(preprocessors[lang])
     lexicon = lexicon.concatenate(lexicon_zh).concatenate(lexicon_zhc)
 
     # build the vocabularies
-    tokenizer, vocab = build_bert_tokenizer(vocab_path, lexicon, cjk=True, revocab=True)
+    tokenizer, vocab = build_bert_tokenizer(vocab_path, lexicon, cjk=cjk, revocab=True)
 
     ### create the tf.Module object for tokenizers & output the result
 
     # build
     tokenizers = tf.Module()
-    tokenizers.zh = CustomTokenizer(RESERVED_TOKENS, vocab_path)
-    tokenizer_path = os.path.join(DIR_TOKEN, 'bert_tokenizers')
+    setattr(tokenizers, lang, CustomTokenizer(RESERVED_TOKENS, vocab_path, cjk=cjk))
+    tokenizer_path = os.path.join(configuration.DIR_TOKEN, 'bert_tokenizers')
 
     # save
     if os.path.isdir(tokenizer_path):
